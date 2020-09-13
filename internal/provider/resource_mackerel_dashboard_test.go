c369caa0df9e0d6cbf3b697f3fdb7c305dc08e4c
KOJIMA Kazunori <kjm.kznr@gmail.com>
2020-09-13 04:14:38 +0000

    Fixed duplicates url_path

--- internal/provider/resource_mackerel_dashboard_test.go
+++ internal/provider/resource_mackerel_dashboard_test.go
@@ -24,7 +24,7 @@ func TestAccMackerelDashboard_Basic(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						"mackerel_dashboard.foobar", "title", rName),
 					resource.TestCheckResourceAttr(
-						"mackerel_dashboard.foobar", "url_path", "foo/bar"),
+						"mackerel_dashboard.foobar", "url_path", fmt.Sprintf("%s/bar/baz", rName)),
 					resource.TestCheckResourceAttr(
 						"mackerel_dashboard.foobar", "body_markdown", "# Head1\n## Head2\n\n* List1\n* List2\n"),
 				),
@@ -47,7 +47,7 @@ func TestAccMackerelDashboard_Update(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						"mackerel_dashboard.foobar", "title", rName),
 					resource.TestCheckResourceAttr(
-						"mackerel_dashboard.foobar", "url_path", "foo/bar"),
+						"mackerel_dashboard.foobar", "url_path", fmt.Sprintf("%s/bar/baz", rName)),
 					resource.TestCheckResourceAttr(
 						"mackerel_dashboard.foobar", "body_markdown", "# Head1\n## Head2\n\n* List1\n* List2\n"),
 				),
@@ -58,7 +58,7 @@ func TestAccMackerelDashboard_Update(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						"mackerel_dashboard.foobar", "title", rName),
 					resource.TestCheckResourceAttr(
-						"mackerel_dashboard.foobar", "url_path", "bar/baz"),
+						"mackerel_dashboard.foobar", "url_path", fmt.Sprintf("%s/bar/baz", rName)),
 					resource.TestCheckResourceAttr(
 						"mackerel_dashboard.foobar", "body_markdown", "# Head1\n## Head2\n\n[Link](https://terraform.io/)\n"),
 				),
@@ -88,7 +88,7 @@ func testAccCheckMackerelDashboardConfigBasic(rName string) string {
 	return fmt.Sprintf(`
 resource "mackerel_dashboard" "foobar" {
   title         = "%s"
-  url_path      = "foo/bar"
+  url_path      = "%s/foo/bar"
 	body_markdown = <<EOF
 # Head1
 ## Head2
@@ -96,19 +96,19 @@ resource "mackerel_dashboard" "foobar" {
 * List1
 * List2
 EOF
-}`, rName)
+}`, rName, rName)
 }
 
 func testAccCheckMackerelDashboardConfigUpdate(rName string) string {
 	return fmt.Sprintf(`
 resource "mackerel_dashboard" "foobar" {
   title         = "%s"
-  url_path      = "bar/baz"
+  url_path      = "%s/bar/baz"
 	body_markdown = <<EOF
 # Head1
 ## Head2
 
 [Link](https://terraform.io/)
 EOF
-}`, rName)
+}`, rName, rName)
 }
