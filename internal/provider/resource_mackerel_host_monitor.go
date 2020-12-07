8188f5aa9f3d0739aac8ba8145787a6e8518e10c
yakult1995 <tet.lv11@gmail.com>
2020-12-07 17:11:50 +0900

    Add Exists func in host_monitor

--- internal/provider/resource_mackerel_host_monitor.go
+++ internal/provider/resource_mackerel_host_monitor.go
@@ -15,6 +15,7 @@ func resourceMackerelHostMonitor() *schema.Resource {
 		Read:   resourceMackerelHostMonitorRead,
 		Update: resourceMackerelHostMonitorUpdate,
 		Delete: resourceMackerelHostMonitorDelete,
+		Exists: resourceMackerelHostMonitorExists,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
@@ -169,6 +170,22 @@ func resourceMackerelHostMonitorUpdate(d *schema.ResourceData, meta interface{})
 
 	log.Printf("[DEBUG] mackerel monitor %q updated.", d.Id())
 	return resourceMackerelHostMonitorRead(d, meta)
+}
+
+func resourceMackerelHostMonitorExists(d *schema.ResourceData, meta interface{}) (b bool, e error) {
+	client := meta.(*mackerel.Client)
+	monitors, err := client.FindMonitors()
+	if err != nil {
+		return false, err
+	}
+
+	for _, m := range monitors {
+		if m.MonitorType() == "host" && m.MonitorID() == d.Id() {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
 
 func resourceMackerelHostMonitorDelete(d *schema.ResourceData, meta interface{}) error {
